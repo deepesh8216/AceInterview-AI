27ae1e547226d29d6611b6d78d013f0066a7183a
deepesh8216 <95678242+deepesh8216@users.noreply.github.com>
2025-07-05 03:11:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -40,9 +40,6 @@ app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
 
 // Start Server
-if(process.env.NODE_ENV !== "production"){
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
 
-export default Server;
