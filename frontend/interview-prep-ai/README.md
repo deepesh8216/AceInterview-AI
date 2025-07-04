b2296e173e46d3979af1bb31ea82ddb9276984af
Deepesh Kumar <kumardeepesh542@gmail.com>
2025-07-05 02:38:50 +0530

    Initial project upload

---
+++ frontend/interview-prep-ai/README.md
@@ -0,0 +1,26 @@
+This app allows users to generate personalized interview questions and answers based on their role and experience, take notes, and review topics with AI-generated explanations.
+
+✨ Key Features
+User Authentication
+Register and log in securely with JWT-based authentication.
+
+Role-Based Interview Sessions
+Generate questions tailored to your job role and experience level.
+
+AI-Powered Q&A
+Automatically create high-quality technical questions and answers using the Gemini API.
+
+Accordion Learning UI
+View Q&A in an expandable, clean interface that supports focused study.
+
+Dynamic AI Explanations
+Get on-demand concept breakdowns and clarifications powered by AI.
+
+Pin Important Questions
+Pin critical questions for quick reference later.
+
+MongoDB Storage
+Save and manage sessions, questions, and notes for future review.
+
+Clean UI with Tailwind CSS
+Enjoy a responsive, modern frontend with a smooth user experience.
---
+++ frontend/interview-prep-ai/src/pages/LandingPage.jsx
@@ -0,0 +1,164 @@
+import React, { useState } from "react";
+
+import HERO_IMG from "../assets/hero-img.png";
+import { APP_FEATURES } from "../utils/data";
+import { useNavigate } from "react-router-dom";
+import { LuSparkles } from "react-icons/lu";
+import Modal from "../components/Modal";
+import Login from "./Auth/Login";
+import SignUp from "./Auth/SignUp";
+import { useContext } from "react";
+import { UserContext } from "../context/userContext";
+import ProfileInfoCard from "../components/Cards/ProfileInfoCard";
+
+const LandingPage = () => {
+  const { user } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const [openAuthModal, setOpenAuthModal] = useState(false);
+  const [currentPage, setCurrentPage] = useState("login");
+
+  const handleCTA = () => {
+    if (!user) {
+      setOpenAuthModal(true);
+    } else {
+      navigate("/dashboard");
+    }
+  };
+
+  return (
+    <>
+      <div className="w-full min-h-full bg-[#E6FFFA]">
+        <div className="w-[500px] h-[500px] bg-teal-300/30 blur-[65px] absolute top-0 left-0" />
+
+        <div className="container mx-auto px-4 pt-6 pb-[200px] relative z-10">
+          {/* Header */}
+          <header className="flex justify-between items-center mb-16">
+            <div className="flex items-center text-xl text-black font-bold">
+              <img src="/vite.png" alt="Logo" className="w-8 h-8 mr-2" />
+              AceInterview AI
+            </div>
+            {user ? (
+              <ProfileInfoCard />
+            ) : (
+              <button
+                className="bg-gradient-to-r from-[#0D9488] to-[#5EEAD4] text-sm font-semibold text-white px-7 py-2.5 rounded-full hover:bg-teal-800 hover:text-white border border-white transition-colors cursor-pointer"
+                onClick={() => setOpenAuthModal(true)}
+              >
+                Login / Sign Up
+              </button>
+            )}
+          </header>
+
+          {/* Hero Content */}
+          <div className="flex flex-col md:flex-row items-center">
+            <div className="w-full md:w-1/2 pr-4 mb-8 md:mb-0">
+              <div className="flex items-center justify-left mb-2">
+                <div className="flex items-center gap-2 text-[13px] text-teal-600 font-semibold bg-teal-100 px-3 py-1 rounded-full border border-teal-300">
+                  <LuSparkles /> AI Powered
+                </div>
+              </div>
+
+              <h1 className="text-5xl text-black font-medium mb-6 leading-tight">
+                Ace Interviews with <br />
+                <span className="text-transparent bg-clip-text bg-[radial-gradient(circle,_#0d9488_0%,_#5eead4_100%)] bg-[length:200%_200%] animate-text-shine font-semibold">
+                  AI-Powered
+                </span>{" "}
+                Learning
+              </h1>
+            </div>
+
+            <div className="w-full md:w-1/2">
+              <p className="text-[17px] text-gray-900 mr-0 md:mr-20 mb-6">
+              Access tailored questions for your role, explore detailed explanations on demand, deepen your understanding of key concepts, and keep your notes organized your way. From gearing up to achieving mastery, this is your all-in-one toolkit for interview success.
+              </p>
+
+              <button
+                className="bg-black text-sm font-semibold text-white px-7 py-2.5 rounded-full hover:bg-teal-200 hover:text-black border border-teal-50 hover:border-teal-400 transition-colors cursor-pointer"
+                onClick={handleCTA}
+              >
+                Get Started
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full min-h-full relative z-10">
+        <div>
+          <section className="flex items-center justify-center -mt-36">
+            <img
+              src={HERO_IMG}
+              alt="Hero Image"
+              className="w-[80vw] rounded-lg"
+            />
+          </section>
+        </div>
+
+        <div className="w-full min-h-full bg-[#E6FFFA] mt-10">
+          <div className="container mx-auto px-4 pt-10 pb-20">
+            <section className="mt-5">
+              <h2 className="text-2xl font-medium text-center mb-12">
+                Features That Make You Shine
+              </h2>
+
+              <div className="flex flex-col items-center gap-8">
+                {/* First 3 cards */}
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-8 w-full">
+                  {APP_FEATURES.slice(0, 3).map((feature) => (
+                    <div
+                      key={feature.id}
+                      className="bg-[#FFFEF8] p-6 rounded-xl shadow-xs hover:shadow-lg shadow-teal-200 transition border border-teal-200"
+                    >
+                      <h3 className="text-base font-semibold mb-3">
+                        {feature.title}
+                      </h3>
+                      <p className="text-gray-600">{feature.description}</p>
+                    </div>
+                  ))}
+                </div>
+
+                {/* Remaining 2 cards */}
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                  {APP_FEATURES.slice(3).map((feature) => (
+                    <div
+                      key={feature.id}
+                      className="bg-[#FFFEF8] p-6 rounded-xl shadow-xs hover:shadow-lg shadow-teal-200 transition border border-teal-200"
+                    >
+                      <h3 className="text-base font-semibold mb-3">
+                        {feature.title}
+                      </h3>
+                      <p className="text-gray-600">{feature.description}</p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+
+        <div className="bg-teal-300/10 backdrop-blur-md rounded-xl border border-teal-300/30 shadow-lg text-sm text-secondary text-center p-5 mt-5">
+          ©Interview Prep AI 2025. All rights reserved.
+        </div>
+      </div>
+
+      <Modal
+        isOpen={openAuthModal}
+        onClose={() => {
+          setOpenAuthModal(false);
+          setCurrentPage("login");
+        }}
+        hideHeader
+      >
+        <div>
+          {currentPage === "login" && <Login setCurrentPage={setCurrentPage} />}
+          {currentPage === "signup" && (
+            <SignUp setCurrentPage={setCurrentPage} />
+          )}
+        </div>
+      </Modal>
+    </>
+  );
+};
+
+export default LandingPage;
